e99cb95a8be8b1a5f6eee42bcf228965df4f46aa
dandelany <dan.delany@gmail.com>
2022-12-12 02:15:29 -0800

    filter out ._foo.jpg files in latest endpoint

--- server/utils/data.mjs
+++ server/utils/data.mjs
@@ -6,7 +6,13 @@ import { DATA_DIR_PATH } from "../constants.mjs";
 export async function getLatestImages(cameraName) {
   const latestDirPath = path.join(DATA_DIR_PATH, `images/${cameraName}/latest`);
   try {
-    return (await readdir(latestDirPath)).filter((imgName) => imgName.endsWith(".jpg")).reverse();
+    return (
+      (await readdir(latestDirPath))
+        // return all jpgs in latest, but filter out ._foo.jpg files created by OS X
+        // https://apple.stackexchange.com/questions/14980/why-are-dot-underscore-files-created-and-how-can-i-avoid-them
+        .filter((imgName) => imgName.endsWith(".jpg") && !imgName.startsWith("._"))
+        .reverse()
+    );
   } catch (err) {
     console.error("error: no latest images", err.toString());
     return [];
