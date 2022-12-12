dd3a43d6ad9af5f2cd92e4a35bda107de150423d
dandelany <dan.delany@gmail.com>
2022-12-11 21:48:09 -0800

    work on ansible server deployment

--- ansible-server/README.md
+++ ansible-server/README.md
@@ -1,1 +1,13 @@
-ansible-playbook -v playbooks/deploy.yml
+## ansible-server
+ansible scripts/roles/etc. for deploying the bloomcatcher server (& web client)
+
+#### Install requirements
+`ansible-galaxy install -r requirements.yml`
+
+#### Deploy to server
+`ansible-playbook -v playbooks/deploy.yml`
+
+# NOTES
+`roles` contains a local copy of `ansible-role-nvm` modified slightly to work with Ansible 7/core 2.14.
+This can be changed to the official version if/when Ansible 7 is supported.
+More info: https://github.com/morgangraphics/ansible-role-nvm/issues/35
--- server/constants.mjs
+++ server/constants.mjs
@@ -7,14 +7,14 @@ export function getDirName() {
   // https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-when-using-the-experimental-modules-flag
   return dirname(fileURLToPath(import.meta.url));
 }
+
+const __dirname = getDirName();
 // path to the base /server folder
-export const baseDirName = getDirName();
+export const baseDirName = __dirname;
 
 // export const HOSTS_FILE = `${__dirname}/../ansible/hosts.yml`;
 
-console.log("__dirname", baseDirName);
 export const DATA_DIR_PATH = path.join(baseDirName, "../data");
-console.log("DATA_DIR_PATH", DATA_DIR_PATH);
 
 // paths on the pi cameras, not local (client) paths
 // export const CAM_REPO_PATH = `/home/pi/bloomcatcher`;
